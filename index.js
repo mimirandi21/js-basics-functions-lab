a57d79e3ebff24476e3ba89ccb1cb21e5d42378e
Randi Schreiner <mimirandi21@gmail.com>
2021-01-24 12:51:11 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(num) {
+  return Math.abs(42 - num);
+}
+
+function distanceFromHqInFeet(num) {
+  return distanceFromHqInBlocks(num) * 264;
+}
+
+function distanceTravelledInFeet(num1, num2) {
+  return Math.abs(num1 - num2) * 264;
+}
+
+function calculatesFarePrice(num1, num2) {
+  let distance = distanceTravelledInFeet(num1, num2);
+  let price = (distanceTravelledInFeet(num1, num2) - 400) * 0.02;
+  if (distance <= 400) {
+    return 0;
+  } else if (distance < 2000) {
+    return price;
+  } else if (distance < 2500) {
+    return 25;
+  } else {
+    return `cannot travel that far`;
+  }
+}
